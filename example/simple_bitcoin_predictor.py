1362f5accf76020253c0770fa14bd1790159a555
Rolv-Arild Braaten <rolv_arild@hotmail.com>
2018-10-31 12:23:03 +0100

    Push

--- example/simple_bitcoin_predictor.py
+++ example/simple_bitcoin_predictor.py
@@ -38,4 +38,4 @@ class SimpleBitcoinPredictor:
         self.loss = tf.losses.mean_squared_error(self.y, logits)
 
         # Accuracy
-        self.accuracy = tf.metrics.precision(self.y, self.f)
+        self.accuracy = tf.metrics.mean_squared_error(self.y, self.f)
